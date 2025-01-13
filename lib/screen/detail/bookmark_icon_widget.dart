a2d61c2183ee355a4742d35f747762bc588d3c7d
penhele <stephenhelk@gmail.com>
2025-01-13 20:57:55 +0700

    first commit

---
+++ lib/screen/detail/bookmark_icon_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tourism_app/data/model/tourism.dart';
+import 'package:tourism_app/provider/detail/bookmark_icon_provider.dart';
+import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
+
+class BookmarkIconWidget extends StatefulWidget {
+  final Tourism tourism;
+  const BookmarkIconWidget({
+    super.key,
+    required this.tourism,
+  });
+
+  @override
+  State<BookmarkIconWidget> createState() => _BookmarkIconWidgetState();
+}
+
+class _BookmarkIconWidgetState extends State<BookmarkIconWidget> {
+  @override
+  void initState() {
+    final bookmarkListProvider = context.read<BookmarkListProvider>();
+    final bookmarkIconProvider = context.read<BookmarkIconProvider>();
+
+    Future.microtask(() {
+      final tourismInList =
+          bookmarkListProvider.checkItemBookmark(widget.tourism);
+      bookmarkIconProvider.isBookmarked = tourismInList;
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        final bookmarkListProvider = context.read<BookmarkListProvider>();
+        final bookmarkIconProvider = context.read<BookmarkIconProvider>();
+        final isBookmarked = bookmarkIconProvider.isBookmarked;
+
+        if (!isBookmarked) {
+          bookmarkListProvider.addBookmark(widget.tourism);
+        } else {
+          bookmarkListProvider.removeBookmark(widget.tourism);
+        }
+        bookmarkIconProvider.isBookmarked = !isBookmarked;
+      },
+      icon: Icon(
+        context.watch<BookmarkIconProvider>().isBookmarked
+          ? Icons.bookmark
+          : Icons.bookmark_outline,
+      ),
+    );
+  }
+}
